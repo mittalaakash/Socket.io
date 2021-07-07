fea3d5094b6127553ee58dbd8579fd44a784120f
mittalaakash <mit.ash.619@gmail.com>
2021-07-07 15:51:07 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', socket => {
+  console.log('what is socket: ', socket);
+  console.log('socket is active to be connected');
+
+  socket.on('chat', payload => {
+    console.log('what is payload: ', payload);
+    io.emit('chat', payload);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server is listening to port 5000...');
+});
